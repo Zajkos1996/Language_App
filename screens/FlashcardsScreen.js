b305b0725956280adf9b065f0f24e9ee4737cb45
matteoxx <mateuszniziolek96@gmail.com>
2019-06-04 22:01:06 +0200

    showing flashcards

--- screens/FlashcardsScreen.js
+++ screens/FlashcardsScreen.js
@@ -1,71 +1,120 @@
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View,TouchableOpacity} from 'react-native';
-import FlipCard from 'react-native-card-flip';
+import React, { Component } from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import FlipCard from "react-native-card-flip";
+import { Button, Icon } from "react-native-elements";
 
-type Props = {};
-export default class App extends Component<Props> {
-    render() {
-        return (
+export default class FlashcardsScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      wordsAndDefinitions: JSON.parse(this.props.wordsAndDefinitions),
+      currentWordAndDefinition: JSON.parse(this.props.wordsAndDefinitions)[0]
+    };
+  }
 
-
-            <View style={styles.container}>
-
-                <FlipCard style={styles.cardContainer} ref={(card) => this.card = card} >
-                    <TouchableOpacity style={styles.card} onPress={() => this.card.flip()} ><Text style={styles.welcome1}>Komputer</Text></TouchableOpacity>
-                    <TouchableOpacity style={styles.card1} onPress={() => this.card.flip()} ><Text style={styles.welcome1}>Computer</Text></TouchableOpacity>
-                </FlipCard>
-            </View>
-        );
+  changeNext = () => {
+    let currentId = this.state.currentWordAndDefinition.id - 1;
+    if (currentId < this.state.wordsAndDefinitions.length - 1) {
+      currentId++;
+      this.setState({
+        currentWordAndDefinition: this.state.wordsAndDefinitions[currentId]
+      });
     }
+  };
+
+  changePrevious = () => {
+    let currentId = this.state.currentWordAndDefinition.id - 1;
+    if (currentId > 0) {
+      currentId--;
+      this.setState({
+        currentWordAndDefinition: this.state.wordsAndDefinitions[currentId]
+      });
+    }
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <FlipCard style={styles.cardContainer} ref={card => (this.card = card)}>
+          <TouchableOpacity
+            style={styles.cardFront}
+            onPress={() => this.card.flip()}
+          >
+            <Text style={styles.cardText}>
+              {this.state.currentWordAndDefinition.wordValue}
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.cardBack}
+            onPress={() => this.card.flip()}
+          >
+            <Text style={styles.cardText}>
+              {this.state.currentWordAndDefinition.definitionValue}
+            </Text>
+          </TouchableOpacity>
+        </FlipCard>
+
+        <View style={styles.buttonsContainer}>
+          <Icon
+            raised
+            name="arrow-left"
+            type="font-awesome"
+            color="#841584"
+            onPress={this.changePrevious}
+          />
+          <Icon
+            raised
+            name="arrow-right"
+            type="font-awesome"
+            color="#841584"
+            onPress={this.changeNext}
+          />
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
-    },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
-    },
-    welcome1: {
-        fontSize: 40,
-        textAlign: 'center',
-        marginTop: '30%',
-    },
-    card: {
-        width: '100%',
-        height: '50%',
-        backgroundColor: '#FE474C',
-        borderRadius: 5,
-        shadowColor: 'rgba(0,0,0,0.5)',
-    },
-    card1: {
-        width: '100%',
-        height: '50%',
-        backgroundColor: '#32CD32',
-        borderRadius: 5,
-        shadowColor: 'rgba(0,0,0,0.5)'
-    },
-    cardContainer: {
-        flex: 1,
-        position:'absolute',
-        left:5,
-        right: 5,
-        bottom: 5,
-        top: 5,
-    },
-    face: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
-    },
-    back: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
-    },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
+  },
+  cardText: {
+    fontSize: 54,
+    color: "#fff"
+  },
+  cardFront: {
+    width: "100%",
+    height: "85%",
+    backgroundColor: "#FE474C",
+    borderRadius: 5,
+    shadowColor: "rgba(0,0,0,0.5)",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  cardBack: {
+    width: "100%",
+    height: "85%",
+    backgroundColor: "#32CD32",
+    borderRadius: 5,
+    shadowColor: "rgba(0,0,0,0.5)",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  cardContainer: {
+    position: "absolute",
+    left: 5,
+    right: 5,
+    top: 15,
+    bottom: 5
+  },
+  buttonsContainer: {
+    position: "absolute",
+    bottom: 5,
+    flexDirection: "row",
+    justifyContent: "center"
+  }
 });
